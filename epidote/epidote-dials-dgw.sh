c786253a953db986294b58a68936909890a6e030
David Waterman <dgwaterman@gmail.com>
2024-02-15 09:23:15 +0000

    Remove the modes of data processing that are not as good. That is, the methods that split the scan at the calibration images using dials.slice_sequence exclude_images_multiple=, do not in general give better results than just dials.integrate exclude_images_multiple=, and are more complicated.
    Also include back the attempt to solve the structure, but refine anyway
    against a completed model

--- epidote/epidote-dials-dgw.sh
+++ epidote/epidote-dials-dgw.sh
@@ -18,38 +18,17 @@ function process-one {
   dials.integrate refined.expt refined.refl prediction.d_min=0.55\
       exclude_images_multiple=$EXCLUDE_IMAGES_MULTIPLE nproc=12\
       profile.gaussian_rs.min_spots.per_degree=0 profile.gaussian_rs.min_spots.overall=0
-
-  # Integrate second way, using scans split at the calibration images
-  dials.slice_sequence exclude_images_multiple=$EXCLUDE_IMAGES_MULTIPLE refined.expt refined.refl
-  dials.integrate refined_sliced.expt refined_sliced.refl prediction.d_min=0.55\
-      profile.gaussian_rs.min_spots.per_degree=0 profile.gaussian_rs.min_spots.overall=0\
-      output.experiments=integrated_sliced.expt output.reflections=integrated_sliced.refl\
-      output.log=dials.integrate.sliced.log nproc=12
-
-  ## Third way, refine again prior to integrating slices.
-  ## Turns out to be fragile, so commenting out!
-  ## This fails for Data_5 with zero reflections for refinement for one of the slices
-  #dials.refine refined_sliced.expt refined_sliced.refl\
-  #    detector.fix=distance crystal.fix=cell\
-  #    output.experiments=re-refined_sliced.expt output.reflections=re-refined_sliced.refl\
-  #    output.log=dials.refine.sliced.log
-  ## This fails for Data_1 and Data_4 with: Something went wrong. Zero pixels selected for estimation of profile parameters.
-  #dials.integrate re-refined_sliced.expt re-refined_sliced.refl prediction.d_min=0.5\
-  #    profile.gaussian_rs.min_spots.per_degree=0 profile.gaussian_rs.min_spots.overall=0\
-  #    output.experiments=integrated_sliced2.expt output.reflections=integrated_sliced2.refl\
-  #    output.log=dials.integrate.sliced2.log nproc=12
 }
 
 function scale_and_solve {
-    INTEGRATED=$1
-    INTENSITY_CHOICE=$2
+    INTENSITY_CHOICE=$1
 
     # Omit Data_1 because it is weak
     dials.scale\
-      ../Data_2/$INTEGRATED.{expt,refl}\
-      ../Data_3/$INTEGRATED.{expt,refl}\
-      ../Data_4/$INTEGRATED.{expt,refl}\
-      ../Data_5/$INTEGRATED.{expt,refl}\
+      ../Data_2/integrated.{expt,refl}\
+      ../Data_3/integrated.{expt,refl}\
+      ../Data_4/integrated.{expt,refl}\
+      ../Data_5/integrated.{expt,refl}\
       d_min=0.55 intensity_choice=$INTENSITY_CHOICE\
       best_unit_cell="9.00      5.83     10.34  90.000 114.921  90.000" # taken from XSCALE.LP, so merging stats are comparable with XDS
 
@@ -59,7 +38,47 @@ function scale_and_solve {
     # Export to dials.hkl
     dials.export scaled.{expt,refl} format=shelx
 
-    # Make dials.ins based on Angelina's xds_a.res
+    # Attempt to solve the structure
+    mkdir -p solve
+    cd solve
+    cp ../dials.hkl .
+    cat <<+ > dials.ins
+TITL P2_1/m
+CELL 0.0251  9.035  5.795 10.397  90.000 115.659  90.000
+ZERR 1.00    0.000  0.000  0.000   0.000   0.000   0.000
+LATT 1
+SYMM -X,Y,-Z
+SFAC SI  2.1293 57.7748  2.5333 16.4756         =
+         0.8349  2.8796  0.3216  0.3860  0.0000 =
+         0.0000  0.0000  0.0000  1.1100 28.0860
+SFAC AL  2.2756 72.3220  2.4280 19.7729         =
+         0.8578  3.0799  0.3166  0.4076  0.0000 =
+         0.0000  0.0000  0.0000  1.1800 26.9815
+SFAC CA  4.4696 99.5228  2.9708 22.6958         =
+         1.9696  4.1954  0.4818  0.4165  0.0000 =
+         0.0000  0.0000  0.0000  1.4000 40.0800
+SFAC FE  2.5440 64.4244  2.3434 14.8806         =
+         1.7588  2.8539  0.5062  0.3502  0.0000 =
+         0.0000  0.0000  0.0000  1.1700 55.8470
+SFAC O   0.4548 23.7803  0.9173  7.6220         =
+         0.4719  2.1440  0.1384  0.2959  0.0000 =
+         0.0000  0.0000  0.0000  0.7300 15.9990
+SFAC H   0.3754 15.4946  0.1408  4.1261         =
+         0.0216  0.0246 -0.1012 46.8840  0.0000 =
+         0.0000  0.0000  0.0000  0.3200  1.0080
+UNIT 3 2 2 1 10 1
+TREF 5000
+HKLF 4
+END
++
+
+    shelxt dials -s"P2(1)_m" > shelxt.log
+    cd ..
+
+    mkdir -p refine
+    cd refine
+    cp ../dials.hkl .
+    # Refine using a dials.ins based on Angelina's xds_a.res
     cat <<EOF > dials.ins
 TITL dials in P2(1)/m
 CELL 0.0251 8.852 5.62 10.185 90 115.517 90
@@ -125,6 +144,7 @@ END
 EOF
 
      shelxl dials > shelxl.log
+     cd ..
 }
 
 # Process each dataset
@@ -158,37 +178,15 @@ cd ..
 # Solve structure with various options
 mkdir -p solve1
 cd solve1/
-scale_and_solve "integrated" "combine"
+scale_and_solve "combine"
 # R1 =  0.2241 for   2847 unique reflections after merging for Fourier
 cd ..
 
 mkdir -p solve2
 cd solve2/
-scale_and_solve "integrated" "profile"
+scale_and_solve "profile"
 # R1 =  0.2249 for   2846 unique reflections after merging for Fourier
 cd ..
 
-mkdir -p solve3
-cd solve3/
-scale_and_solve "integrated_sliced" "combine"
-# R1 =  0.2226 for   2844 unique reflections after merging for Fourier
-cd ..
-
-mkdir -p solve4
-cd solve4/
-scale_and_solve "integrated_sliced" "profile"
-# R1 =  0.2304 for   2817 unique reflections after merging for Fourier
-cd ..
-
-# The below jobs fail, because of failures to process in Data_1, Data_4 and Data_5
-#mkdir -p solve5
-#cd solve5/
-#scale_and_solve "integrated_sliced2" "combine"
-#cd ..
-#
-#mkdir -p solve6
-#cd solve6/
-#scale_and_solve "integrated_sliced2" "profile"
-#cd ..
 
 
